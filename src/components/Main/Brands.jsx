6b9e80faee5f2ab7761d22ae53012fdbb46a1d4d
developedbyrayhan <islamraihanofficial@gmail.com>
2023-10-21 19:19:22 +0600

    updated

--- src/components/Main/Brands.jsx
+++ src/components/Main/Brands.jsx
@@ -12,7 +12,7 @@ const Brands = () => {
 
     return (
         <div className='container mx-auto my-20'>
-            <h1 className='text-xl text-[#002B44] md:text-3xl lg:text-4xl font-bold text-center'>Premium Brand Showcase</h1>
+            <h1 className='text-[#002B44] text-3xl lg:text-4xl font-bold text-center'>Premium Brand Showcase</h1>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center px-10 lg:px-0 mt-8'>
                 {
                     allBrands?.map((brand) => <BrandCard key={brand._id} brand={brand}></BrandCard>)
