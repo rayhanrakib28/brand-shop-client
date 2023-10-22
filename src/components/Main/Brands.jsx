b4c7a89e9b062a97a9a11195d34b3e53874a0e3c
developedbyrayhan <islamraihanofficial@gmail.com>
2023-10-22 14:13:22 +0600

    updated

--- src/components/Main/Brands.jsx
+++ src/components/Main/Brands.jsx
@@ -3,21 +3,31 @@ import BrandCard from "./BrandCard";
 import { useEffect } from "react";
 
 const Brands = () => {
-    const [allBrands, setAllBrands] = useState([]);
+    const [allBrands, setAllBrands] = useState();
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         fetch('https://tech-shop-server-ecru.vercel.app/brands')
             .then(res => res.json())
-            .then(data => setAllBrands(data))
+            .then(data => {
+                setAllBrands(data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setIsLoading(false);
+            });
     }, []);
 
     return (
         <div className='container mx-auto my-20'>
             <h1 className='text-[#002B44] text-3xl lg:text-4xl font-bold text-center'>Premium Brand Showcase</h1>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center px-10 lg:px-0 mt-8'>
-                {
-                    allBrands?.map((brand) => <BrandCard key={brand._id} brand={brand}></BrandCard>)
-                }
-            </div>
+            {isLoading ? (
+                <p className="h-10 flex justify-center items-center">Loading...</p>
+            ) : (
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center px-10 lg:px-0 mt-8'>
+                    {allBrands.map(brand => <BrandCard key={brand._id} brand={brand} />)}
+                </div>
+            )}
         </div>
     );
 };
